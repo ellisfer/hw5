30b41d9b5fb5b7a0e1c4d67f0224de51a45f9044
Ellis Fertig <efertig@usc.edu>
2025-04-25 22:42:49 +0000

    finished hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,10 +21,38 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool isValid(int day, int id, const size_t dailyNeed, const size_t maxShifts, vector<int>& shiftsWorked, DailySchedule& sched);
 
+bool schedHelper(int day, unsigned int worker, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, vector<int>& shiftsWorked, DailySchedule& sched);
+
+void printShifts(const vector<int>& shiftsWorked);
+// type for the ID of a worker
+typedef unsigned int Worker_T;
+// n-by-k Matrix of each of the k workers' availability over an n-day period
+typedef std::vector<std::vector<bool>> AvailabilityMatrix;
+// n-by-d matrix with the d worker IDs who are scheduled 
+// to work on each of the n days
+typedef std::vector<std::vector<Worker_T> > DailySchedule;
 
 // Add your implementation of schedule() and other helper functions here
 
+/**
+ * @brief Produces a work schedule given worker availability,
+ *        the number of needed workers per day, and the maximum 
+ *        shifts any single worker is allowed. Returns true if
+ *        and the valid schedule if a solution exists, and false
+ *        otherwise. 
+ * 
+ * @param [in]  avail n x k vector of vectors (i.e. matrix) of the availability
+ *                    of the k workers for each of the n days
+ * @param [in]  dailyNeed Number of workers needed per day (aka d)
+ * @param [in]  maxShifts Maximum shifts any worker is allowed over 
+ *                        the n day period (aka m)
+ * @param [out] sched n x d vector of vectors indicating the d workers
+ *                    who are scheduled to work on each of the n days
+ * @return true if a solution exists; sched contains the solution
+ * @return false if no solution exists; sched is undefined (can be anything)
+ */
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -38,8 +66,64 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    vector<int> shiftsWorked(avail[0].size(), 0);
+
+    for(unsigned int i = 0; i < avail.size(); i++){
+      sched.push_back(vector<unsigned int>(dailyNeed, avail[0].size()));
+    }
+
+    return (schedHelper(0, 0, avail, dailyNeed, maxShifts, shiftsWorked, sched));
 
 
 
 }
 
+void printShifts(const vector<int>& shiftsWorked){
+  for(unsigned int i = 0; i < shiftsWorked.size(); i++){
+    cout << shiftsWorked[i] << " ";
+  }
+  cout << endl;
+}
+
+bool isValid(int day, int id, const size_t dailyNeed, const size_t maxShifts, vector<int>& shiftsWorked, DailySchedule& sched){
+  if(shiftsWorked[id] > maxShifts){
+    //cout << "isValid: false. DAY: " << day << " ID: " << id << endl;
+    return false;
+  };
+//cout << "isValid: true. DAY: " << day << " ID: " << id << endl;
+  return true;
+}
+
+bool schedHelper(int day, unsigned int worker, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, vector<int>& shiftsWorked, DailySchedule& sched){
+  // base case: made it to end, scheduling  
+  //cout << "Helper. DAY: " << day << " WORKER: " << worker << endl;
+  //cout <<  "   shiftsWorked "; printShifts(shiftsWorked); 
+  if(day == avail.size()) return true;
+
+  int ogval = sched[day][worker];
+  for(unsigned int i = 0; i < avail[0].size(); i++){
+    if(avail[day][i] == 0 || (find(sched[day].begin(), sched[day].end(), i) != sched[day].end())) continue;
+    sched[day][worker] = i;
+    if(shiftsWorked[i] < maxShifts){
+      //cout << "   VALID: " << i << endl;
+      shiftsWorked[i] ++;
+      if(worker != dailyNeed-1){
+        if(schedHelper(day, worker +1, avail, dailyNeed, maxShifts, shiftsWorked, sched)) return true;
+      }
+      else{
+        if(schedHelper(day+1, 0, avail, dailyNeed, maxShifts, shiftsWorked, sched)) return true;
+      }
+      shiftsWorked[i] --;
+      //cout << "BACTRACKING: Day: " << day << " worker " << worker << " i: " << i << endl;
+    }
+      //cout << "   INVALID: " << i << endl;
+      sched[day][worker] = ogval;
+  
+  }
+
+  if(ogval == avail[0].size()){
+    sched[day][worker] = ogval;
+  }
+
+  return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,15 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(
+  std::string input,
+  std::string options, int start, std::set<std::string>& added,
+  std::set<std::string>& words, const std::set<std::string>& dict
+);
+
+void swap(std::string& s, int first, int second);
+
+void addWord(std::string in, std::string& options, unsigned int ind, int optionind, std::set<std::string>& words, const std::set<std::string>& dict);
 
 
 // Definition of primary wordle function
@@ -22,7 +31,110 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+   
+    // step 1: count n, make set, copy of in, make array of floating and empty 
+    int len = in.length();
+    int floatlen = floating.length();
 
+    // cout << "in wordle! " << endl;
+    // cout << in << " " << floating << "   " << len << " " << floatlen << endl;
+    std::set<std::string> words;
+    std::set<std::string> added;
+
+    std::string options = floating;
+    std::string input = in;
+    int empty = 0;
+
+    for(unsigned int i = 0; i < len; i++){
+      if(in[i] == '-'){
+        if(empty >= floatlen){
+          options += '-';
+        }
+        empty ++;
+      }
+    }
+    //cout << "options: " << options << endl;
+    wordleHelper(input, options, 0, added, words, dict);
+
+    return words;
+}
+
+void wordleHelper(
+  std::string input,
+  std::string options, int start, std::set<std::string>& added, std::set<std::string>& words, const std::set<std::string>& dict
+){
+  //cout << "helper: " << input << " " << options << " " << start << endl;
+  if(options.length() == 0){
+    addWord(input, options, 0, 0, words, dict);
+    return;
+  }
+  if(start == options.length()-1) {
+    if(options.length() == 1){
+      addWord(input, options, 0, 0, words, dict);
+    }
+     return;}
+
+  if(start == 0) {
+    //cout << "2 " << endl; 
+    addWord(input, options, 0, 0, words, dict); 
+    //cout << "done ? " << endl;
+  }
+  std::string permute = options;
+  //cout << "3" << endl;
+  for(unsigned int i = options.length()-1; i >= start; i--){
+    //cout << " i = " << i << endl;
+    wordleHelper(input, permute, start+1, added, words, dict);
+    //cout << "Swapping: " << permute << " "<< start << " " << i;
+    if(i == start) continue;
+    swap(permute, start, i);
+   // cout << "permuted: " << permute << endl;
+    if(added.find(permute) == added.end() ){
+      added.insert(permute);
+      addWord(input, permute, 0, 0, words, dict);
+    }
+    
+  }
+}
+
+void swap(std::string& s, int first, int second){
+  if(!(first < s.length() && second < s.length())){
+   // std::cout << "string swap indexing error" << std::endl;
+    return;
+  }
+  char temp = s[first];
+  s[first] = s[second];
+  s[second] = temp;
+} 
+
+void addWord(std::string in, std::string& options, unsigned int ind, int optionind, std::set<std::string>& words, const std::set<std::string>& dict){
+    //cout << "adding " << options << endl;
+    //cout << "ok ind =  " << ind << " letter = " << in[ind] << " len = " << in.length() << endl;
+    // final word
+    if(ind == in.length()){
+
+      //std::cout << "going to add " <<  in << std::endl;
+      if(dict.find(in) != dict.end()){
+        //cout << "real word! " << in << endl;
+        words.insert(in);
+      }
+      return;
+    }
+    //cout <<" word not done! " << endl;
+    if(in[ind] == '-'){
+      if(options[optionind] != '-'){
+        in[ind] = options[optionind];
+        addWord(in, options, ind+1, optionind + 1, words, dict);
+      }
+      else{
+        for(char l = 97; l < 123; l++){
+          in[ind] = l;
+          addWord(in, options, ind+1, optionind+1, words, dict);
+        }
+      }
+  }
+  else{
+    addWord(in, options, ind+1, optionind, words, dict);
+  }
 }
 
 // Define any helper functions here
